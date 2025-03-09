4ef20e04b6c2a0039542c9d8b426788127281c97
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-03-09 21:04:25 +0900

    feat: add home activity

---
+++ app/src/main/java/kr/co/langquence/presentation/ui/home/HomeActivity.kt
@@ -0,0 +1,27 @@
+package kr.co.langquence.presentation.ui.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import kr.co.langquence.presentation.theme.AppTheme
+
+@AndroidEntryPoint
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
