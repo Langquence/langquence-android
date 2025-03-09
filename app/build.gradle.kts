d04cd17174dc04f98d745337616b92277fbe1374
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-03-09 21:01:58 +0900

    chore: rollback logback dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,6 +106,7 @@ dependencies {
     // logger
     implementation("org.slf4j:slf4j-api:2.0.7")
     implementation("io.github.oshai:kotlin-logging-android:7.0.5")
+    implementation("com.github.tony19:logback-android:3.0.0")
 }
 
 // Allow references to generated code
