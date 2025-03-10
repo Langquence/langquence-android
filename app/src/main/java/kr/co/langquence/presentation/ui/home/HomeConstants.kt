18cc8aca7b08b4c810d9a635e9c9e1d7b4db195b
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-03-10 21:54:39 +0900

    feat: divide constants from home screen

---
+++ app/src/main/java/kr/co/langquence/presentation/ui/home/HomeConstants.kt
@@ -0,0 +1,20 @@
+package kr.co.langquence.presentation.ui.home
+
+import androidx.compose.ui.graphics.Color
+
+object HomeConstants {
+    // 텍스트
+    const val TEXT_MAIN = "메인"
+    const val TEXT_VOICE_RECOGNITION = "메인 (음성 듣기)"
+    const val TEXT_NAVIGATE_TO_PROFILE = "프로필로 이동"
+
+    // 크기
+    val MAIN_CONTAINER_SIZE = 240
+    val MIC_BUTTON_SIZE = 100
+    val BOTTOM_BAR_HEIGHT = 48
+
+    // 색상
+    val BORDER_COLOR_NORMAL = Color.White
+    val BORDER_COLOR_LISTENING = Color(0xFF4A90E2)
+    val BOTTOM_BAR_COLOR = Color(0xFF333333)
+}
