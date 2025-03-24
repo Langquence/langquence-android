abd740498c2c51414956b61b1159cd8d8fac6005
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-03-24 22:02:02 +0900

    fix: apply interface to correct repository

--- app/src/main/java/kr/co/langquence/common/di/RepositoryModule.kt
+++ app/src/main/java/kr/co/langquence/common/di/RepositoryModule.kt
@@ -1,17 +1,19 @@
 package kr.co.langquence.common.di
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kr.co.langquence.data.remote.CorrectApi
 import kr.co.langquence.data.repository.CorrectRepositoryImpl
+import kr.co.langquence.model.repository.CorrectRepository
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class RepositoryModule {
+abstract class RepositoryModule {
 	@Singleton
-	@Provides
-	fun provideCorrectRepository(api: CorrectApi): CorrectRepositoryImpl = CorrectRepositoryImpl(api)
+	@Binds
+	abstract fun bindCorrectRepository(
+		correctRepositoryImpl: CorrectRepositoryImpl
+	): CorrectRepository
 }
