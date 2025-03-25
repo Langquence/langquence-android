c3044e4d1919ea5d9fd1e7ab9fc5298e0c7be173
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-03-25 17:56:47 +0900

    feat: impl usecase

---
+++ app/src/main/java/kr/co/langquence/model/usecase/CorrectUseCase.kt
@@ -0,0 +1,27 @@
+package kr.co.langquence.model.usecase
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kr.co.langquence.model.domain.CorrectAnswer
+import kr.co.langquence.model.domain.Resource
+import kr.co.langquence.model.repository.CorrectRepository
+import javax.inject.Inject
+
+private val log = KotlinLogging.logger {}
+
+class CorrectUseCase @Inject constructor(
+    private val repository: CorrectRepository
+) {
+    operator fun invoke(bytes: ByteArray): Flow<Resource<CorrectAnswer>> = flow {
+        try {
+            emit(Resource.Loading())
+            val response = repository.request(bytes)
+            emit(Resource.Success(response))
+        } catch (e: Exception) {
+            log.error { "Error occurred: ${e.message}" }
+
+            emit(Resource.Error(e.message))
+        }
+    }
+}
