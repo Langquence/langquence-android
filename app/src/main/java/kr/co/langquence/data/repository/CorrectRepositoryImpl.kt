3ec8c4e7f63a50148a419b38e9c3a4a3507bb921
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-03-27 14:40:01 +0900

    fix: convert byte stream type to request body for avoding json serialize

--- app/src/main/java/kr/co/langquence/data/repository/CorrectRepositoryImpl.kt
+++ app/src/main/java/kr/co/langquence/data/repository/CorrectRepositoryImpl.kt
@@ -3,10 +3,16 @@ package kr.co.langquence.data.repository
 import kr.co.langquence.data.remote.CorrectApi
 import kr.co.langquence.model.domain.CorrectAnswer
 import kr.co.langquence.model.repository.CorrectRepository
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
 import javax.inject.Inject
 
 class CorrectRepositoryImpl @Inject constructor(
-	private val api: CorrectApi
-): CorrectRepository {
-	override suspend fun request(body: ByteArray): CorrectAnswer = api.request(body).toEntity()
+    private val api: CorrectApi
+) : CorrectRepository {
+    override suspend fun request(body: ByteArray): CorrectAnswer {
+        val requestBody = body.toRequestBody("application/octet-stream".toMediaTypeOrNull())
+
+        return api.request(requestBody).toEntity()
+    }
 }
