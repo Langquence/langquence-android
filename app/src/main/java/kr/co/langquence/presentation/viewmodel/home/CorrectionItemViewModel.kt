305042c2d1f00566cfb348532f34087cdc734a7f
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-04-08 17:26:29 +0900

    feat: correction item view model

---
+++ app/src/main/java/kr/co/langquence/presentation/viewmodel/home/CorrectionItemViewModel.kt
@@ -0,0 +1,44 @@
+package kr.co.langquence.presentation.viewmodel.home
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kr.co.langquence.model.domain.CorrectAnswer
+import kr.co.langquence.model.usecase.CorrectUseCase
+import javax.inject.Inject
+
+// @TODO: View에 보여줄 데이터로 가공해야 함.
+data class CorrectionItem(
+    val id: Long,
+    val original: String,
+    val needsCorrection: Boolean,
+    val corrected: String,
+    val explanation: String,
+    val alternatives: List<String>
+) {
+    companion object {
+        fun fromDomain(domain: CorrectAnswer): CorrectionItem {
+            return CorrectionItem(
+                id = domain.id,
+                original = domain.original,
+                needsCorrection = domain.needsCorrection,
+                corrected = domain.corrected,
+                explanation = domain.explanation,
+                alternatives = domain.alternatives
+            )
+        }
+    }
+}
+
+@HiltViewModel
+class CorrectionItemViewModel @Inject constructor(
+    private val correctUseCase: CorrectUseCase
+) {
+    private val _items = MutableStateFlow<List<CorrectionItem>>(emptyList())
+    val items: StateFlow<List<CorrectionItem>> = _items.asStateFlow()
+
+    fun saveItem(item: CorrectionItem) {
+        _items.value += item
+    }
+}
